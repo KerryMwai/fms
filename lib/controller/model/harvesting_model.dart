3f6ca618448ad22782b6a6afd9831d8280d718b6
KerryMwai <kerrywangamati@gmail.com>
2023-07-01 12:17:57 +0300

    Working on the harvesting_model

--- lib/controller/model/harvesting_model.dart
+++ lib/controller/model/harvesting_model.dart
@@ -2,11 +2,14 @@ class HarvestingModel{
   final String field;
   final String crop;
   final int workforce;
+  final String workload;
   final String harvestingmethod;
   final String storage;
+  final String harvestingseason;
   final DateTime timefrom;
   final DateTime timeTo;
   final String skills;
+  final double quantityestimate;
 
-  HarvestingModel(this.field, this.crop, this.workforce, this.harvestingmethod, this.storage, this.timefrom, this.timeTo, this.skills);
+  HarvestingModel(this.field, this.crop, this.workforce, this.harvestingmethod, this.storage, this.timefrom, this.timeTo, this.skills, this.workload, this.harvestingseason, this.quantityestimate);
 }
