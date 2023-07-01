a51f03bca7182d6c48b10b8b31ce9dc8f0888386
KerryMwai <kerrywangamati@gmail.com>
2023-07-01 12:14:43 +0300

    Harvesting model complete

--- lib/controller/model/harvesting_model.dart
+++ lib/controller/model/harvesting_model.dart
@@ -4,4 +4,9 @@ class HarvestingModel{
   final int workforce;
   final String harvestingmethod;
   final String storage;
+  final DateTime timefrom;
+  final DateTime timeTo;
+  final String skills;
+
+  HarvestingModel(this.field, this.crop, this.workforce, this.harvestingmethod, this.storage, this.timefrom, this.timeTo, this.skills);
 }
