a29c5fd9888331d9537ceb2e619214aaabf71ce4
KerryMwai <kerrywangamati@gmail.com>
2023-07-01 22:15:12 +0300

    Completed toJson and fromJson method on harvesting plan model

--- lib/controller/model/harvest_plan_model.dart
+++ lib/controller/model/harvest_plan_model.dart
@@ -9,4 +9,21 @@ class HarvestPlan{
   final DateTime?harvestingdate;
 
   HarvestPlan({required this.crop, required this.method, required this.season, required this.quantity, required this.equipment, required this.labor, required this.storage, required this.harvestingdate});
+
+  factory HarvestPlan.fromJson(Map<String, dynamic>json){
+    return HarvestPlan(crop: json['crop'], method: json['method'], season: json['season'], quantity: json['quantity'].toDouble(), equipment: json['equipment'], labor: json['labor'], storage: json['storage'], harvestingdate: json['harvestingdate'].toDate());
+  }
+
+  Map<String, dynamic> toJson(){
+    return {
+      'crop':crop,
+      'method':method,
+      'season':season,
+      'quantity':quantity,
+      'equipment':equipment,
+      'labor':labor,
+      'storage':storage,
+      'harvestingdate':harvestingdate
+    };
+  }
 }
