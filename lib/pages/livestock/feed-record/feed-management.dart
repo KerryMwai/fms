aa63982e8798ae8a013bf4455e93e1f8bc167d46
KerryMwai <kerrywangamati@gmail.com>
2023-07-02 13:57:23 +0300

    Feed record under livestock management adding logo complete

--- lib/pages/livestock/feed-record/feed-management.dart
+++ lib/pages/livestock/feed-record/feed-management.dart
@@ -15,82 +15,135 @@ class FeedManagementPage extends StatefulWidget {
 class _FeedManagementPageState extends State<FeedManagementPage> {
   @override
   Widget build(BuildContext context) {
-    Size size=MediaQuery.of(context).size;
-    return  Padding(
+    Size size = MediaQuery.of(context).size;
+    return Padding(
       padding: const EdgeInsets.all(15.0),
       child: GridView.builder(
           itemCount: 4,
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 20
-            ), 
-        itemBuilder: (context, index){
-          if(index==0){
-            return GestureDetector(
-                          onTap: () {
-                           Navigator.push(context, MaterialPageRoute(builder: (_)=>const InventoryLevels()));
-                          },
-                          child: Column(
-                            children: [
-                              Container(
-                                height: size.width * 0.33,
-                                decoration: BoxDecoration(
-    
-                                    image:const  DecorationImage(
-                                        image: AssetImage("asset/images/harvesting.jpg"), fit: BoxFit.cover),
-                                    borderRadius: BorderRadius.circular(10)),
-                              ),
-                              const SizedBox(
-                                height: 20,
-                              ),
-                              Text(
-                                "Feed Inventory levels",
-                                style: TextStyle(
-                                    fontSize: 17,
-                                    fontWeight: FontWeight.w500,
-                                    color: green),
-                              )
-                            ],
-                          ),
-                        );
-            return GestureDetector(
-              onTap: ()=>Navigator.push(context, MaterialPageRoute(builder: (_)=>const InventoryLevels())),
-              child: const Card(
-              margin: EdgeInsets.all(10),
-              color: Colors.grey,
-              child: Center(child: Text("Feed Inventory levels")),
+              crossAxisCount: 2, crossAxisSpacing: 10, mainAxisSpacing: 20),
+          itemBuilder: (context, index) {
+            if (index == 0) {
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) => const InventoryLevels()));
+                },
+                child: Column(
+                  children: [
+                    Container(
+                      height: size.width * 0.33,
+                      decoration: BoxDecoration(
+                          image: const DecorationImage(
+                              image: AssetImage("asset/images/record.jpg"),
+                              fit: BoxFit.cover),
+                          borderRadius: BorderRadius.circular(10)),
                     ),
-            );
-          }else if(index==1){
-            return GestureDetector(
-              onTap: ()=>Navigator.push(context, MaterialPageRoute(builder: (_)=>const PurchaseFeed())),
-              child: const Card(
-              margin: EdgeInsets.all(10),
-              color: Colors.grey,
-              child: Center(child: Text("Purchase Feed")),
+                    const SizedBox(
+                      height: 20,
                     ),
-            );
-          }else if(index==2){
-            return GestureDetector(
-              onTap: ()=>Navigator.push(context, MaterialPageRoute(builder: (_)=>const FeedUsage())),
-              child: const Card(
-              margin: EdgeInsets.all(10),
-              color: Colors.grey,
-              child: Center(child: Text("Feed usage")),
+                    Text(
+                      "Feed Inventory levels",
+                      style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.w500,
+                          color: green),
+                    )
+                  ],
+                ),
+              );
+            } else if (index == 1) {
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (_) => const PurchaseFeed()));
+                },
+                child: Column(
+                  children: [
+                    Container(
+                      height: size.width * 0.33,
+                      decoration: BoxDecoration(
+                          image: const DecorationImage(
+                              image: AssetImage("asset/images/purchase.png"),
+                              fit: BoxFit.cover),
+                          borderRadius: BorderRadius.circular(10)),
                     ),
-            );
-          }else{
-            return GestureDetector(
-              onTap: ()=>Navigator.push(context, MaterialPageRoute(builder: (_)=>const AddNewFeed())),
-              child: const Card(
-              margin: EdgeInsets.all(10),
-              color: Colors.grey,
-              child: Center(child: Text("Add new")),
+                    const SizedBox(
+                      height: 20,
                     ),
-            );
-          }
-        }),
+                    Text(
+                      "Purchase Feed",
+                      style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.w500,
+                          color: green),
+                    )
+                  ],
+                ),
+              );
+            } else if (index == 2) {
+                return GestureDetector(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (_) => const FeedUsage()));
+                },
+                child: Column(
+                  children: [
+                    Container(
+                      height: size.width * 0.33,
+                      decoration: BoxDecoration(
+                          image: const DecorationImage(
+                              image: AssetImage("asset/images/level.jpg"),
+                              fit: BoxFit.cover),
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      "Feed usage",
+                      style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.w500,
+                          color: green),
+                    )
+                  ],
+                ),
+              );
+            } else {
+               return GestureDetector(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (_) => const AddNewFeed()));
+                },
+                child: Column(
+                  children: [
+                    Container(
+                      height: size.width * 0.33,
+                      decoration: BoxDecoration(
+                          image: const DecorationImage(
+                              image: AssetImage("asset/images/add.png"),
+                              fit: BoxFit.cover),
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      "Add new",
+                      style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.w500,
+                          color: green),
+                    )
+                  ],
+                ),
+              );
+             
+            }
+          }),
     );
   }
 }
