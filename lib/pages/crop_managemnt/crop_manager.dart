92cf17b18aff49b56891d860c763f06d4a0ffe49
KerryMwai <kerrywangamati@gmail.com>
2023-07-02 13:10:04 +0300

    Completed adding logos to the cropmanager file section

--- lib/pages/crop_managemnt/crop_manager.dart
+++ lib/pages/crop_managemnt/crop_manager.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fms/dammies/constants.dart';
 import 'package:fms/pages/crop_managemnt/crop/records_growth_stages.dart';
 import 'package:fms/pages/crop_managemnt/crop/view_crop_plans.dart';
 import 'package:fms/pages/crop_managemnt/field/alerts_and_notifications.dart';
@@ -15,84 +16,181 @@ class CropManager extends StatefulWidget {
 class _CropManagerState extends State<CropManager> {
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-        itemCount: 5,
-        gridDelegate:
-            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-        itemBuilder: (context, index) {
-          if (index == 1) {
-            return GestureDetector(
-              onTap: () => Navigator.push(context,
-                  MaterialPageRoute(builder: (_) => const CropPlansPage())),
-              child: const Card(
-                margin: EdgeInsets.all(10),
-                color: Colors.grey,
-                child: Padding(
-                  padding: EdgeInsets.all(12.0),
-                  child: Center(child: Text("View Crop Plans")),
+    Size size = MediaQuery.of(context).size;
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: GridView.builder(
+          itemCount: 5,
+          gridDelegate:
+              const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                childAspectRatio: 2/3
                 ),
-              ),
-            );
-          }else if (index == 2) {
-            return GestureDetector(
-              onTap: () => Navigator.push(context,
-                  MaterialPageRoute(builder: (_) => CropGrowthRecords())),
-              child: const Card(
-                margin: EdgeInsets.all(10),
-                color: Colors.grey,
-                child: Padding(
-                  padding: EdgeInsets.all(12.0),
-                  child: Center(child: Text("Crop Growth Records")),
-                ),
-              ),
-            );
-          } else if (index == 3) {
-            return GestureDetector(
-              onTap: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (_) => const FieldInformationPage())),
-              child: const Card(
-                margin: EdgeInsets.all(10),
-                color: Colors.grey,
-                child: Padding(
-                  padding: EdgeInsets.all(12.0),
-                  child: Center(child: Text("Field Information")),
-                ),
-              ),
-            );
-          } else if (index == 4) {
-            return GestureDetector(
-              onTap: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (_) => const WeatherInformation())),
-              child: const Card(
-                margin: EdgeInsets.all(10),
-                color: Colors.grey,
-                child: Padding(
-                  padding: EdgeInsets.all(12.0),
-                  child: Center(child: Text("Weather Information")),
-                ),
-              ),
-            );
-          } else {
-            return GestureDetector(
-              onTap: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (_) => CropsAlertNotificationTable())),
-              child: const Card(
-                margin: EdgeInsets.all(10),
-                color: Colors.grey,
-                child: Padding(
-                  padding: EdgeInsets.all(12.0),
-                  child: Center(child: Text("Alerts and Notifications")),
-                ),
-              ),
-            );
-          }
-        });
+          itemBuilder: (context, index) {
+            if (index == 1) {
+                 return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                const CropPlansPage()));
+                  },
+                  child: Column(
+                    children: [
+                      Container(
+                        height: size.width * 0.33,
+                        decoration: BoxDecoration(
+                            image: const DecorationImage(
+                                image: AssetImage("asset/images/plans.jpg"),
+                                fit: BoxFit.cover),
+                            borderRadius: BorderRadius.circular(10)),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "View Crop Plans",
+                        style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.w500,
+                            color: green),
+                      )
+                    ],
+                  ),
+                );
+            }else if (index == 2) {
+              return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                CropGrowthRecords()));
+                  },
+                  child: Column(
+                    children: [
+                      Container(
+                        height: size.width * 0.33,
+                        decoration: BoxDecoration(
+                            image: const DecorationImage(
+                                image: AssetImage("asset/images/record.jpg"),
+                                fit: BoxFit.cover),
+                            borderRadius: BorderRadius.circular(10)),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "Crop Growth Records",
+                        style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.w500,
+                            color: green),
+                      )
+                    ],
+                  ),
+                );
+            } else if (index == 3) {
+               return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                const FieldInformationPage()));
+                  },
+                  child: Column(
+                    children: [
+                      Container(
+                        height: size.width * 0.33,
+                        decoration: BoxDecoration(
+                            image: const DecorationImage(
+                                image: AssetImage("asset/images/farminfor.jpg"),
+                                fit: BoxFit.cover),
+                            borderRadius: BorderRadius.circular(10)),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "Field Information",
+                        style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.w500,
+                            color: green),
+                      )
+                    ],
+                  ),
+                );
+            } else if (index == 4) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                const WeatherInformation()));
+                  },
+                  child: Column(
+                    children: [
+                      Container(
+                        height: size.width * 0.33,
+                        decoration: BoxDecoration(
+                            image: const DecorationImage(
+                                image: AssetImage("asset/images/weather.jpg"),
+                                fit: BoxFit.cover),
+                            borderRadius: BorderRadius.circular(10)),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "Weather Information",
+                        style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.w500,
+                            color: green),
+                      )
+                    ],
+                  ),
+                );
+            } else {
+                 return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                CropsAlertNotificationTable()));
+                  },
+                  child: Column(
+                    children: [
+                      Container(
+                        height: size.width * 0.33,
+                        decoration: BoxDecoration(
+                            image: const DecorationImage(
+                                image: AssetImage("asset/images/alert.jpg"),
+                                fit: BoxFit.cover),
+                            borderRadius: BorderRadius.circular(10)),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "Alerts and Notifications",
+                        style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.w500,
+                            color: green),
+                      )
+                    ],
+                  ),
+                );
+            }
+          }),
+    );
   }
 }
 // CropsAlertNotificationTable
