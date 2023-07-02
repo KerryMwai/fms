ef0e65737ef50473a8c5d0f00fe7eff783033ff0
KerryMwai <kerrywangamati@gmail.com>
2023-07-02 18:32:11 +0300

    Provided a scaffold for the weather information module

--- lib/pages/livestock/breeding/breed_management.dart
+++ lib/pages/livestock/breeding/breed_management.dart
@@ -14,111 +14,113 @@ class BreedManagement extends StatefulWidget {
 class _BreedManagementState extends State<BreedManagement> {
   @override
   Widget build(BuildContext context) {
-    Size size=MediaQuery.of(context).size;
-    return  Padding(
+    Size size = MediaQuery.of(context).size;
+    return Padding(
       padding: const EdgeInsets.all(15.0),
       child: GridView.builder(
           itemCount: 3,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,mainAxisSpacing: 20, crossAxisSpacing: 10, childAspectRatio: 2/2.5), 
-        itemBuilder: (context, index){
-          if(index==1){
-    
-             return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (_) => const AnimalReproductivityAndHistory()));
-                      },
-                      child: Column(
-                        children: [
-                          Container(
-                            height: size.width * 0.33,
-                            decoration: BoxDecoration(
-                                image: const DecorationImage(
-                                    image: AssetImage("asset/images/record.jpg"),
-                                    fit: BoxFit.cover),
-                                borderRadius: BorderRadius.circular(10)),
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          Text(
-                            "Breed Productivity History",
-                            style: TextStyle(
-                                fontSize: 17,
-                                fontWeight: FontWeight.w500,
-                                color: green),
-                          )
-                        ],
-                      ),
-                    );
-          }else if(index==2){
-    
-     return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (_) => const AnimalWeightManagement()));
-                      },
-                      child: Column(
-                        children: [
-                          Container(
-                            height: size.width * 0.33,
-                            decoration: BoxDecoration(
-                                image: const DecorationImage(
-                                    image: AssetImage("asset/images/laborm.jpg"),
-                                    fit: BoxFit.cover),
-                                borderRadius: BorderRadius.circular(10)),
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          Text(
-                            "Weight Management",
-                            style: TextStyle(
-                                fontSize: 17,
-                                fontWeight: FontWeight.w500,
-                                color: green),
-                          )
-                        ],
-                      ),
-                    );
-          }else{
-             return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (_) => AnimalWeightAnalytics()));
-                      },
-                      child: Column(
-                        children: [
-                          Container(
-                            height: size.width * 0.33,
-                            decoration: BoxDecoration(
-                                image: const DecorationImage(
-                                    image: AssetImage("asset/images/level.jpg"),
-                                    fit: BoxFit.cover),
-                                borderRadius: BorderRadius.circular(10)),
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          Text(
-                            "Weight Analytics",
-                            style: TextStyle(
-                                fontSize: 17,
-                                fontWeight: FontWeight.w500,
-                                color: green),
-                          )
-                        ],
-                      ),
-                    );
-    
-          }
-        }),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 20,
+              crossAxisSpacing: 10,
+              childAspectRatio: 2 / 2.5),
+          itemBuilder: (context, index) {
+            if (index == 1) {
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) =>
+                              const AnimalReproductivityAndHistory()));
+                },
+                child: Column(
+                  children: [
+                    Container(
+                      height: size.width * 0.33,
+                      decoration: BoxDecoration(
+                          image: const DecorationImage(
+                              image: AssetImage("asset/images/record.jpg"),
+                              fit: BoxFit.cover),
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      "Breed Productivity History",
+                      style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.w500,
+                          color: green),
+                    )
+                  ],
+                ),
+              );
+            } else if (index == 2) {
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) => const AnimalWeightManagement()));
+                },
+                child: Column(
+                  children: [
+                    Container(
+                      height: size.width * 0.33,
+                      decoration: BoxDecoration(
+                          image: const DecorationImage(
+                              image: AssetImage("asset/images/laborm.jpg"),
+                              fit: BoxFit.cover),
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      "Weight Management",
+                      style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.w500,
+                          color: green),
+                    )
+                  ],
+                ),
+              );
+            } else {
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) => AnimalWeightAnalytics()));
+                },
+                child: Column(
+                  children: [
+                    Container(
+                      height: size.width * 0.33,
+                      decoration: BoxDecoration(
+                          image: const DecorationImage(
+                              image: AssetImage("asset/images/level.jpg"),
+                              fit: BoxFit.cover),
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      "Weight Analytics",
+                      style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.w500,
+                          color: green),
+                    )
+                  ],
+                ),
+              );
+            }
+          }),
     );
   }
 }
