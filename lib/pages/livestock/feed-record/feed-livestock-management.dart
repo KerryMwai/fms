df70bb12004764f88c1ad94071274bc819f4d94c
KerryMwai <kerrywangamati@gmail.com>
2023-07-03 12:06:49 +0300

    Completed adding the navigation the add the schedule

--- lib/pages/livestock/feed-record/feed-livestock-management.dart
+++ lib/pages/livestock/feed-record/feed-livestock-management.dart
@@ -3,126 +3,125 @@ import 'package:fms/dammies/constants.dart';
 
 import 'feed-livestock-management/add_livestock_feed_type.dart';
 import 'feed-livestock-management/feed_consumption_history.dart';
-import 'feed-livestock-management/add_feed_schedule.dart';
+import 'feed-livestock-management/feed_schedule_information.dart';
 
 class LivestockFeedManagement extends StatefulWidget {
   const LivestockFeedManagement({super.key});
 
   @override
-  State<LivestockFeedManagement> createState() => _LivestockFeedManagementState();
+  State<LivestockFeedManagement> createState() =>
+      _LivestockFeedManagementState();
 }
 
 class _LivestockFeedManagementState extends State<LivestockFeedManagement> {
   @override
   Widget build(BuildContext context) {
-     Size size=MediaQuery.of(context).size;
-    return  Padding(
+    Size size = MediaQuery.of(context).size;
+    return Padding(
       padding: const EdgeInsets.all(15.0),
       child: GridView.builder(
           itemCount: 3,
-          
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            mainAxisSpacing: 20,
-            crossAxisSpacing: 10,
-            childAspectRatio: 2/2.5,
-            crossAxisCount: 2), 
-        itemBuilder: (context, index){
-          if(index==0){
-               return GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (_) => const AddLiveStockFeedType()));
-                    },
-                    child: Column(
-                      children: [
-                        Container(
-                          height: size.width * 0.33,
-                          decoration: BoxDecoration(
-                              image: const DecorationImage(
-                                  image: AssetImage("asset/images/record.jpg"),
-                                  fit: BoxFit.cover),
-                              borderRadius: BorderRadius.circular(10)),
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Text(
-                          "Listock and its type of feed",
-                          style: TextStyle(
-                              fontSize: 17,
-                              fontWeight: FontWeight.w500,
-                              color: green),
-                        )
-                      ],
+              mainAxisSpacing: 20,
+              crossAxisSpacing: 10,
+              childAspectRatio: 2 / 2.5,
+              crossAxisCount: 2),
+          itemBuilder: (context, index) {
+            if (index == 0) {
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) => const AddLiveStockFeedType()));
+                },
+                child: Column(
+                  children: [
+                    Container(
+                      height: size.width * 0.33,
+                      decoration: BoxDecoration(
+                          image: const DecorationImage(
+                              image: AssetImage("asset/images/record.jpg"),
+                              fit: BoxFit.cover),
+                          borderRadius: BorderRadius.circular(10)),
                     ),
-                  );
-          }else if(index==1){
-                return GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (_) =>const FeedConsumptionHistory()));
-                    },
-                    child: Column(
-                      children: [
-                        Container(
-                          height: size.width * 0.33,
-                          decoration: BoxDecoration(
-                              image: const DecorationImage(
-                                  image: AssetImage("asset/images/level.jpg"),
-                                  fit: BoxFit.cover),
-                              borderRadius: BorderRadius.circular(10)),
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Text(
-                          "Feed consumption History",
-                          style: TextStyle(
-                              fontSize: 17,
-                              fontWeight: FontWeight.w500,
-                              color: green),
-                        )
-                      ],
+                    const SizedBox(
+                      height: 20,
                     ),
-                  );
-          
-          }else {
-            return GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (_) =>const FeedingSchedule()));
-                    },
-                    child: Column(
-                      children: [
-                        Container(
-                          height: size.width * 0.33,
-                          decoration: BoxDecoration(
-                              image: const DecorationImage(
-                                  image: AssetImage("asset/images/add.png"),
-                                  fit: BoxFit.cover),
-                              borderRadius: BorderRadius.circular(10)),
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Text(
-                          "Setup Feeding Schedule",
-                          style: TextStyle(
-                              fontSize: 17,
-                              fontWeight: FontWeight.w500,
-                              color: green),
-                        )
-                      ],
+                    Text(
+                      "Listock and its type of feed",
+                      style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.w500,
+                          color: green),
+                    )
+                  ],
+                ),
+              );
+            } else if (index == 1) {
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) => const FeedConsumptionHistory()));
+                },
+                child: Column(
+                  children: [
+                    Container(
+                      height: size.width * 0.33,
+                      decoration: BoxDecoration(
+                          image: const DecorationImage(
+                              image: AssetImage("asset/images/level.jpg"),
+                              fit: BoxFit.cover),
+                          borderRadius: BorderRadius.circular(10)),
                     ),
-                  );
-          }
-        }),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      "Feed consumption History",
+                      style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.w500,
+                          color: green),
+                    )
+                  ],
+                ),
+              );
+            } else {
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) => const FeedScheduleInformation()));
+                },
+                child: Column(
+                  children: [
+                    Container(
+                      height: size.width * 0.33,
+                      decoration: BoxDecoration(
+                          image: const DecorationImage(
+                              image: AssetImage("asset/images/add.png"),
+                              fit: BoxFit.cover),
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      "Setup Feeding Schedule",
+                      style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.w500,
+                          color: green),
+                    )
+                  ],
+                ),
+              );
+            }
+          }),
     );
   }
 }
