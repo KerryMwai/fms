ee845faf47e5dea21be06cc6211bb8921a0a7b3f
KerryMwai <kerrywangamati@gmail.com>
2023-07-03 11:33:24 +0300

    Designing a feed schedule model

--- lib/repository/livestock_repository.dart
+++ lib/repository/livestock_repository.dart
@@ -8,4 +8,10 @@ class LivestockRepostory{
   Future<void> updateFeed(id, feed)=>firestore.collection("feeds").doc(id).update(feed);
   Stream<QuerySnapshot> getAllFeedsSnapshot()=>firestore.collection("feeds").snapshots();
   Future<void> deleteFeed(id)=>firestore.collection("feeds").doc(id).delete();
+
+  // Livestock feeding schedule
+  Future<void> addFeedSchedule(feedschedule)=>firestore.collection("feed-schedules").add(feedschedule);
+  Future<void> updateFeedSchedule(id, feedchedule)=>firestore.collection("feed-schedules").doc(id).update(feedchedule);
+  Stream<QuerySnapshot> getAllFeedSchedulesSnapshot()=>firestore.collection("feedschedules").snapshots();
+  Future<void> deleteFeedSchedule(id)=>firestore.collection("feed-schedules").doc(id).delete();
 }
