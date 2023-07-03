283c11c01535ff3caf7f1111c7b5b1cabe535e7b
KerryMwai <kerrywangamati@gmail.com>
2023-07-03 18:20:12 +0300

    Added a model for the animal productove

---
+++ lib/controller/model/animal_fertility_and_reproductive_history_model.dart
@@ -0,0 +1,75 @@
+
+class FertilityAndReproductiveHistoryModel {
+  final String animalid;
+  final String animalbreed;
+  final DateTime birthdate;
+  final int breedingattempt;
+  final int breedingsuccess;
+  final String reproductivecycle;
+  final String estrusbehaviour;
+  final DateTime conceptiondate;
+  final int gestationperiod;
+  final String reproductivehealth;
+  final double breedingperformance;
+  final String reproductiveinterventions;
+  final String observations;
+  final String breedingprogram;
+  final String matingschedule;
+
+  FertilityAndReproductiveHistoryModel(
+      {required this.animalid,
+      required this.animalbreed,
+      required this.birthdate,
+      required this.breedingattempt,
+      required this.breedingsuccess,
+      required this.reproductivecycle,
+      required this.estrusbehaviour,
+      required this.conceptiondate,
+      required this.gestationperiod,
+      required this.reproductivehealth,
+      required this.breedingperformance,
+      required this.reproductiveinterventions,
+      required this.observations,
+      required this.breedingprogram,
+      required this.matingschedule});
+
+  factory FertilityAndReproductiveHistoryModel.fromJson(
+      Map<String, dynamic> json) {
+    return FertilityAndReproductiveHistoryModel(
+        animalid: json['animalid'],
+        animalbreed: json['animalbreed'],
+        birthdate: json['birthdate'],
+        breedingattempt: json['breedingattempt'],
+        breedingsuccess: json['breedingsuccess'],
+        reproductivecycle: json['reproductivecycle'],
+        estrusbehaviour: json['estrusbehaviour'],
+        conceptiondate: json['conceptiondate'],
+        gestationperiod: json['gestationperiod'],
+        reproductivehealth: json['reproductivehealth'],
+        breedingperformance: json['breedingperformance'],
+        reproductiveinterventions: json['reproductiveinterventions'],
+        observations: json['observations'],
+        breedingprogram: json['breedingprogram'],
+        matingschedule: json['matingschedule']);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'animalid': animalid,
+      'animalbreed': animalbreed,
+      'birthdate': birthdate,
+      'breedingattempt': breedingattempt,
+      'breedingsuccess': breedingsuccess,
+      'reproductivecycle': reproductivecycle,
+      'estrusbehaviour': estrusbehaviour,
+      'conceptiondate': conceptiondate,
+      'gestationperiod': gestationperiod,
+      'reproductivehealth': reproductivehealth,
+      'breedingperformance': breedingperformance,
+      'observations': observations,
+      'breedingprogram': breedingprogram,
+      'matingschedule': matingschedule,
+      'reproductiveinterventions':reproductiveinterventions
+    };
+  }
+}
--- lib/pages/livestock/breeding/breedig-management/add_breed.dart
+++ lib/pages/livestock/breeding/breedig-management/add_breed.dart
@@ -10,62 +10,117 @@ class AddBreed extends StatefulWidget {
 }
 
 class _AddBreedState extends State<AddBreed> {
-  final acquisitiondate=TextEditingController();
-  final breedattempts=TextEditingController();
-  final breedingsuccess=TextEditingController();
-  final reproductivecycle=TextEditingController();
-  final heat=TextEditingController();
-  final matingschedule=TextEditingController();
-  final conceptiondate=TextEditingController();
-  final gestationperiod=TextEditingController();
-  final reproductivehealth=TextEditingController();
-  final breedingperformance=TextEditingController();
-  final interventions=TextEditingController();
-  final reproductvehistoryofparents=TextEditingController(); 
-  final observation=TextEditingController();
-  final _formKey=GlobalKey<FormState>();
+  final animalid = TextEditingController();
+  final animalbreed = TextEditingController();
+  final acquisitiondate = TextEditingController();
+  final breedattempts = TextEditingController();
+  final breedingsuccess = TextEditingController();
+  final reproductivecycle = TextEditingController();
+  final heat = TextEditingController();
+  final matingschedule = TextEditingController();
+  final conceptiondate = TextEditingController();
+  final gestationperiod = TextEditingController();
+  final reproductivehealth = TextEditingController();
+  final breedingperformance = TextEditingController();
+  final interventions = TextEditingController();
+  final reproductvehistoryofparents = TextEditingController();
+  final observation = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    Size size=MediaQuery.of(context).size;
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: blueGrey,
         title: const Text("Add  Breed"),
         centerTitle: true,
       ),
-      body: Padding(padding: const EdgeInsets.all(15), child: Form(
-        key: _formKey,
-        child: ListView(
-          children: [
-             FeedFormField3(controller: acquisitiondate,labelText: "Breeding acquisition date",valitationText: "Breeding acquisition date is required"),
-             FeedFormField2(controller: breedattempts,labelText: "Breeding attempts",valitationText: "Breeding attempts is required"),
-             FeedFormField2(controller: breedingsuccess,labelText: "Breeding success",valitationText: "Breeding success is required"),
-             FeedFormField(controller: reproductivecycle,labelText: "Breed reproductive cycle",valitationText: "Breed reproductive cycle is required"),
-             FeedFormField(controller: heat,labelText: "Breed heat",valitationText: "Breed heat is required"),
-             FeedFormField(controller: matingschedule,labelText: "Breed mating schedule",valitationText: "Breed mating schedule is required"),
-             FeedFormField3(controller: conceptiondate,labelText: "Breeding conception date",valitationText: "Breeding conception date is required"),
-             FeedFormField2(controller: gestationperiod,labelText: "Breed gestation period in days",valitationText: "Breed gestation period is required"),
-             FeedFormField(controller: reproductivehealth,labelText: "Breed reproductive health",valitationText: "Breed reproductive health is required"),
-             FeedFormField2(controller: breedingperformance,labelText: "Breed performance as percent",valitationText: "Breed perfomance is required"),
-             FeedFormField(controller: interventions,labelText: "Breed reproductive intervention",valitationText: "Breed reproductive intervention is required"),
-              FeedFormField(controller: reproductvehistoryofparents,labelText: "Reproductive history of parents",valitationText: "Reproductive history of parents is required"),
-              FeedFormField(controller: observation,labelText: "Breed observation",valitationText: "Breed observation is required"),
-            
-            
-           Expanded(child:   Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(width: size.width*0.4,
-                child: ElevatedButton(onPressed: (){
-                  if(_formKey.currentState!.validate()){
-            
-                  }
-                }, child:const Text("Add Breed")),)
-              ],
-             ))
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              FeedFormField(
+                  controller: animalid,
+                  labelText: "Breed id",
+                  valitationText: "Breed id is required"),
+              FeedFormField(
+                  controller: animalbreed,
+                  labelText: "Breed name",
+                  valitationText: "Breed name is required"),
+              FeedFormField3(
+                  controller: acquisitiondate,
+                  labelText: "Breeding acquisition date",
+                  valitationText: "Breeding acquisition date is required"),
+              FeedFormField2(
+                  controller: breedattempts,
+                  labelText: "Breeding attempts",
+                  valitationText: "Breeding attempts is required"),
+              FeedFormField2(
+                  controller: breedingsuccess,
+                  labelText: "Breeding success",
+                  valitationText: "Breeding success is required"),
+              FeedFormField(
+                  controller: reproductivecycle,
+                  labelText: "Breed reproductive cycle",
+                  valitationText: "Breed reproductive cycle is required"),
+              FeedFormField(
+                  controller: heat,
+                  labelText: "Breed heat",
+                  valitationText: "Breed heat is required"),
+              FeedFormField(
+                  controller: matingschedule,
+                  labelText: "Breed mating schedule",
+                  valitationText: "Breed mating schedule is required"),
+              FeedFormField3(
+                  controller: conceptiondate,
+                  labelText: "Breeding conception date",
+                  valitationText: "Breeding conception date is required"),
+              FeedFormField2(
+                  controller: gestationperiod,
+                  labelText: "Breed gestation period in days",
+                  valitationText: "Breed gestation period is required"),
+              FeedFormField(
+                  controller: reproductivehealth,
+                  labelText: "Breed reproductive health",
+                  valitationText: "Breed reproductive health is required"),
+              FeedFormField2(
+                  controller: breedingperformance,
+                  labelText: "Breed performance as percent",
+                  valitationText: "Breed perfomance is required"),
+              FeedFormField(
+                  controller: interventions,
+                  labelText: "Breed reproductive intervention",
+                  valitationText:
+                      "Breed reproductive intervention is required"),
+              FeedFormField(
+                  controller: reproductvehistoryofparents,
+                  labelText: "Reproductive history of parents",
+                  valitationText:
+                      "Reproductive history of parents is required"),
+              FeedFormField(
+                  controller: observation,
+                  labelText: "Breed observation",
+                  valitationText: "Breed observation is required"),
+              Expanded(
+                  child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: size.width * 0.4,
+                    child: ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {}
+                        },
+                        child: const Text("Add Breed")),
+                  )
+                ],
+              ))
+            ],
+          ),
         ),
-      ),),
+      ),
     );
   }
 }
