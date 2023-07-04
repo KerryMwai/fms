921c828177cd56dd80a88c2d9f924389323abd24
KerryMwai <kerrywangamati@gmail.com>
2023-07-04 22:18:14 +0300

    Added weight model, styled the add weight button and added the routing to the add weight

--- lib/pages/livestock/breeding/breedig-management/add_animal_weight.dart
+++ lib/pages/livestock/breeding/breedig-management/add_animal_weight.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:fms/controller/model/animal_weight_model.dart';
 import 'package:fms/dammies/constants.dart';
+import 'package:fms/pages/livestock/breeding/breedig-management/weight_management.dart';
+import 'package:fms/repository/livestock_repository.dart';
 import 'package:fms/widgets/feed_widgets/custom_form_field.dart';
 
 class AddWeight extends StatefulWidget {
@@ -11,7 +14,7 @@ class AddWeight extends StatefulWidget {
 
 class _AddWeightState extends State<AddWeight> {
   final animalId = TextEditingController();
-  final weightdate = TextEditingController();
+  DateTime? weightdate;
   final weight = TextEditingController();
   final weighttype = TextEditingController();
   final remarks = TextEditingController();
@@ -35,10 +38,40 @@ class _AddWeightState extends State<AddWeight> {
                   controller: animalId,
                   labelText: "Animal id",
                   valitationText: "Animal id is required"),
-              FeedFormField3(
-                  controller: weightdate,
-                  labelText: "Weight date",
-                  valitationText: "Weight date is required"),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 10),
+                child: TextFormField(
+                  onTap: () async {
+                    final DateTime? picked = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2023),
+                      lastDate: DateTime(2040),
+                    );
+                    setState(() {
+                      weightdate = picked ?? DateTime.now();
+                    });
+                  },
+                  readOnly: true,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Weight measurement date',
+                  ),
+                  validator: (value) {
+                    // ignore: unnecessary_null_comparison
+                    if (weightdate == null) {
+                      return 'Please select weight measurement date';
+                    }
+                    return null;
+                  },
+                  controller: TextEditingController(
+                    // ignore: unnecessary_null_comparison
+                    text: weightdate != null
+                        ? weightdate.toString().split(' ')[0]
+                        : '',
+                  ),
+                ),
+              ),
               FeedFormField2(
                   controller: weight,
                   labelText: "Weight (Kg)",
@@ -61,7 +94,15 @@ class _AddWeightState extends State<AddWeight> {
                         style: ButtonStyle(
                             backgroundColor: MaterialStatePropertyAll(green)),
                         onPressed: () {
-                          if (_formKey.currentState!.validate()) {}
+                          if (_formKey.currentState!.validate()) {
+                            LivestockRepostory().addAnimalWeight(
+                                AnimalWeightModel(
+                                    animalid: animalId.text,
+                                    weightdate: weightdate,
+                                    weight: double.parse(weight.text),
+                                    weighttype: weighttype.text,
+                                    remarks: remarks.text)).then((value) => ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Weight added successfully")))).then((value) => Navigator.push(context, MaterialPageRoute(builder: (_)=>const AnimalWeightManagement())));
+                          }
                         },
                         child: const Text("Add Weight")),
                   )
